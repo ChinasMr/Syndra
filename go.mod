2db4b48a1b3dc537d32d6629b0ac6a4be86318b7
Daemon <974105109@qq.com>
2022-01-04 11:39:53 +0800

    feat: add the hello package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ChinasMr/Syndra
+
+go 1.17
---
+++ hello/hello_test.go
@@ -0,0 +1,27 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "Hello World",
+			args: args{name: "World"},
+			want: "Hello World!",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Hello(tt.args.name); got != tt.want {
+				t.Errorf("Hello() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
